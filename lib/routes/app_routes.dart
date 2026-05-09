16a3370ece3285aa266b987cdbcd1d72759952c0
injam <injamamulh544@gmail.com>
2026-05-09 14:41:58 +0600

    add routes file

---
+++ lib/routes/app_routes.dart
@@ -0,0 +1,18 @@
+class AppRoutes {
+  static const String signInScreen = '/sign_in_screen';
+  static const String signUpScreen = '/sign_up_screen';
+  static const String forgotPasswordScreen = '/forgot_password_screen';
+  static const String forgotVerifyOtpScreen = '/forgot_verify_otp_screen';
+  static const String resetPasswordScreen = '/reset_password_screen';
+  static const String signUpOtpScreen = '/sign_up_otp_screen';
+
+  // Profile screens
+  static const String aboutUsScreen = '/about_us_screen';
+  static const String changePasswordScreen = '/change_password_screen';
+  static const String contactSupportScreen = '/contact_support_screen';
+  static const String editProfileScreen = '/edit_profile_screen';
+  static const String faqScreen = '/faq_screen';
+  static const String languageScreen = '/language_screen';
+  static const String privacyPolicyScreen = '/privacy_policy_screen';
+  static const String termsAndConditionsScreen = '/terms_and_conditions_screen';
+}
---
+++ lib/routes/app_routes_file.dart
@@ -0,0 +1,64 @@
+import 'package:get/get.dart';
+import 'app_routes.dart';
+import 'authBindings.dart';
+
+// Import screens
+import '../screens/baseScreen/authScreen/sign_in/sign_in_screen.dart';
+import '../screens/baseScreen/authScreen/sign_up/sign_up_screen.dart';
+import '../screens/baseScreen/authScreen/forgotPasswordScreen/forgorPasswordScreen.dart';
+import '../screens/baseScreen/authScreen/forgotVerifyOtpScreen/forgotVeriyOtpScreen.dart';
+import '../screens/baseScreen/authScreen/resetPasswordScreen/resetPasswordScreen.dart';
+import '../screens/baseScreen/authScreen/signUpVerifyOtpScreen/SignUpOtpScreen.dart';
+
+import '../screens/profileScreen/aboutUs/aboutUsScreen.dart';
+import '../screens/profileScreen/changePassword/changePasswordScreen.dart';
+import '../screens/profileScreen/contactSupport/contactSupportScreen.dart';
+import '../screens/profileScreen/editProfile/editProfileScreen.dart';
+import '../screens/profileScreen/faq/faqScreen.dart';
+import '../screens/profileScreen/language/languageScreen.dart';
+import '../screens/profileScreen/privacyPolicy/privacyPolicyScreen.dart';
+import '../screens/profileScreen/termsAndconditions/termsAndconditionsScreen.dart';
+
+class AppRoutesFile {
+  static final List<GetPage> routes = [
+    GetPage(
+      name: AppRoutes.signInScreen,
+      page: () => const SignInScreen(),
+      binding: AuthBindings(),
+    ),
+    GetPage(
+      name: AppRoutes.signUpScreen,
+      page: () => const SignUpScreen(),
+      binding: AuthBindings(),
+    ),
+    GetPage(
+      name: AppRoutes.forgotPasswordScreen,
+      page: () => const ForgotPasswordScreen(),
+      binding: AuthBindings(),
+    ),
+    GetPage(
+      name: AppRoutes.forgotVerifyOtpScreen,
+      page: () => const ForgotVerifyOtpScreen(),
+      binding: AuthBindings(),
+    ),
+    GetPage(
+      name: AppRoutes.resetPasswordScreen,
+      page: () => const ResetPasswordScreen(),
+      binding: AuthBindings(),
+    ),
+    GetPage(
+      name: AppRoutes.signUpOtpScreen,
+      page: () => const SignUpOtpScreen(),
+      binding: AuthBindings(),
+    ),
+    // Profile pages
+    GetPage(name: AppRoutes.aboutUsScreen, page: () => const AboutUsScreen()),
+    GetPage(name: AppRoutes.changePasswordScreen, page: () => const ChangePasswordScreen()),
+    GetPage(name: AppRoutes.contactSupportScreen, page: () => const ContactSupportScreen()),
+    GetPage(name: AppRoutes.editProfileScreen, page: () => const EditProfileScreen()),
+    GetPage(name: AppRoutes.faqScreen, page: () => const FaqScreen()),
+    GetPage(name: AppRoutes.languageScreen, page: () => const LanguageScreen()),
+    GetPage(name: AppRoutes.privacyPolicyScreen, page: () => const PrivacyPolicyScreen()),
+    GetPage(name: AppRoutes.termsAndConditionsScreen, page: () => const TermsAndconditionsScreen()),
+  ];
+}
